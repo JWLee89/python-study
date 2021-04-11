272e9dafddc61122e8548be2eab93550ccd6442c
JWLee89 <ljay189@gmail.com>
2021-04-11 15:00:27 +0900

    Added file context example

--- src/common/with_statement.py
+++ src/common/with_statement.py
@@ -1,4 +1,5 @@
 
+
 def with_example():
     with open('dummy.txt', 'r') as file:
         print(file.read())
@@ -12,5 +13,31 @@ def with_example():
             file.close()
 
 
+# From Python Tricks. Really good example
+class ManagedFile:
+    def __init__(self, name, mode):
+        self.name = name
+        self.mode = mode
+
+    def __enter__(self):
+        """
+        Returned when using with ManagedFile(name, 'r') as file
+        :return: the file (output of open
+        :rtype:
+        """
+        self.file = open(self.name, self.mode)
+        print(f"Opened file ... {self.name} in '{self.mode}' mode")
+        return self.file
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if self.file:
+            print("File closing ... yee ... ")
+            self.file.close()
+
+
 if __name__ == "__main__":
     with_example()
+
+    # We can create our own context class
+    with ManagedFile('dummy.txt', 'r') as file:
+        print(file.read())
