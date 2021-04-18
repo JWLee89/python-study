d1db9ac39336a3db5bf6654b2b0136db82af0cab
Jay Lee <ljay189@gmail.com>
2021-04-18 15:27:48 +0900

    Updated example on named_tuples.py to better show intent of named tuple

--- src/datastructures/named_tuples.py
+++ src/datastructures/named_tuples.py
@@ -9,3 +9,12 @@ Person = namedtuple("Person", ["age", "height"])
 if __name__ == "__main__":
     bob = Person(28, 180)
     print(bob)
+    print(bob.age)
+
+    # error
+    # Named tuples follow the "write once, read many times convention"
+    try:
+        bob.age = 100
+        print("Named tuples are immutable")
+    except AttributeError as ex:
+        print(ex)
