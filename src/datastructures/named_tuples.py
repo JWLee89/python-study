1f559af8a4c9da339276539e6795eb90003c618e
Jay Lee <ljay189@gmail.com>
2021-04-20 19:03:04 +0900

    Added example on utilizing sets

--- src/datastructures/named_tuples.py
+++ src/datastructures/named_tuples.py
@@ -6,6 +6,19 @@ For example the following
 """
 Person = namedtuple("Person", ["age", "height"])
 
+
+"""
+Since named Person are a python class, we can also extend them
+"""
+print(type(Person))
+
+
+class Employee(Person):
+    def __init__(self, age, height, salary):
+        super().__init__(age, height)
+        self.salary = salary
+
+
 if __name__ == "__main__":
     bob = Person(28, 180)
     print(bob)
@@ -18,3 +31,5 @@ if __name__ == "__main__":
         print("Named tuples are immutable")
     except AttributeError as ex:
         print(ex)
+
+    dan = Employee(28, 180, )
